bbf6498b3e2ce6f4a325187ee2b02b9c8d0b210b
Bones <ben@properdesign.co.uk>
2024-10-01 16:45:30 +0100

    Adds prettier and formats

--- resources/views/archive-event.blade.php
+++ resources/views/archive-event.blade.php
@@ -1,11 +1,11 @@
 @extends('layouts.app')
 
 @section('content')
-<h1 class="text-3xl text-center p-10">Events</h1>
-<ol class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 p-4">
-
-  @while(have_posts()) @php(the_post())
-    @include('partials.event-teaser')
-  @endwhile
-</ol>
+  <h1 class="text-3xl text-center p-10">Events</h1>
+  <ol class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 p-4">
+    @while (have_posts())
+      @php(the_post())
+      @include('partials.event-teaser')
+    @endwhile
+  </ol>
 @endsection
--- resources/views/partials/event-teaser.blade.php
+++ resources/views/partials/event-teaser.blade.php
@@ -1,11 +1,20 @@
-<li class="list-item transform transition duration-1000 opacity-0 translate-y-8" >
-    <article>
-    <a href=@php(the_permalink()) >
-
-    <img class="w-full object-contain transform transition duration-500 hover:scale-105 py-2" src=@php(the_post_thumbnail_url())>
-    <div class="py-2 text-s text-violet-700">
-      <datetime date="2024-05-08">@php(the_field('start_date_and_time'))<datetime/> - @php(the_field('end_date_and_time'))</div>
-    <h2 class="py-2 text-xl font-serif">@php(the_title())</h2>
+<li
+  class="list-item transform transition duration-1000 opacity-0 translate-y-8"
+>
+  <article>
+    <a href="@php(the_permalink())">
+      <img
+        class="w-full object-contain transform transition duration-500 hover:scale-105 py-2"
+        src="@php(the_post_thumbnail_url())"
+      />
+      <div class="py-2 text-s text-violet-700">
+        <datetime date="2024-05-08">
+          @php(the_field('start_date_and_time'))
+        </datetime>
+        -
+        @php(the_field('end_date_and_time'))
+      </div>
+      <h2 class="py-2 text-xl font-serif">@php(the_title())</h2>
     </a>
   </article>
-  </li>
+</li>
