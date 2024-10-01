597df82309b56b20c9615b2ad3b443d7a326efdd
Bones <ben@properdesign.co.uk>
2024-10-01 15:30:30 +0100

    Updates proxy url for HMR

--- bud.config.js
+++ bud.config.js
@@ -34,7 +34,7 @@ export default async (app) => {
    */
   app
     .setUrl('http://localhost:3000')
-    .setProxyUrl('http://example.test')
+    .setProxyUrl('http://test-site.local')
     .watch(['resources/views', 'app']);
 
   /**
